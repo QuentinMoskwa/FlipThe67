c497c536663afff438785a262cbe34317b2439a2
QuentinMoskwa <95235691+QuentinMoskwa@users.noreply.github.com>
2026-03-24 11:05:16 +0100

    feat : config proxy, cors et healthcheck #7 (#16)
    * feat : config proxy, cors et healthcheck #7
    
    * chore : update gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,11 @@ export default defineConfig({
     proxy: {
       "/socket.io": {
         target: "http://localhost:3001",
-        ws: true, // 👈 indispensable pour que les WebSockets passent par Vite
+        ws: true,
+      },
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
       },
     },
   },
